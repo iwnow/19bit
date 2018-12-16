d4cde79c349812c0a87372b7afa6030dd98d22c6
savushkinandrew <savushkinandrew@gmail.com>
2018-12-16 11:55:36 +0300

    firebase config

--- mobile/src/environments/environment.ts
+++ mobile/src/environments/environment.ts
@@ -4,7 +4,15 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBO5g3TgjXc6n9bcP0G9z4VSE98tW7y8bo',
+    authDomain: 'fcc-book-trading-173021.firebaseapp.com',
+    databaseURL: 'https://fcc-book-trading-173021.firebaseio.com',
+    projectId: 'fcc-book-trading-173021',
+    storageBucket: 'fcc-book-trading-173021.appspot.com',
+    messagingSenderId: '763399536402'
+  }
   
 };
 
