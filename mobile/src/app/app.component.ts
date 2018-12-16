c102e7820e843b64221c781d55fc041def64fecb
savushkinandrew <savushkinandrew@gmail.com>
2018-12-16 11:50:46 +0300

    some bad changes

--- mobile/src/app/app.component.ts
+++ mobile/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  firebase: {
+    apiKey: 'AIzaSyBO5g3TgjXc6n9bcP0G9z4VSE98tW7y8bo',
+    authDomain: 'fcc-book-trading-173021.firebaseapp.com',
+    databaseURL: 'https://fcc-book-trading-173021.firebaseio.com',
+    projectId: 'fcc-book-trading-173021',
+    storageBucket: 'fcc-book-trading-173021.appspot.com',
+    messagingSenderId: '763399536402'
+  }
 }
+
